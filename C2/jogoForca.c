06eb50669a673689497b517c5527c1484799f860
vinifillmann <vinifillmann08@gmail.com>
2022-08-05 20:35:09 -0300

    Avançando no curso

--- C2/jogoForca.c
+++ C2/jogoForca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char palavraSecreta[20];
@@ -10,6 +11,11 @@ int main() {
     while (!acertou && !enforcou) {
         char chute;
         scanf("%c", &chute);
+        for (int i = 0; i < strlen(palavraSecreta); i++) {
+            if (palavraSecreta[i] == chute) {
+                printf("A posicao %d tem essa letra!\n", i);
+            }
+        }
         
     }
 }
